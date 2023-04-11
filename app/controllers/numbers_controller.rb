de765ca3c568e67a49e6a90d15b49fbda6e0ec78
Hazel Chui <hazelchui@uchicago.edu>
2023-04-11 14:16:02 +0000

    finished part 1

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,17 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+def losers
+  @unlucky = Array.new
+
+  5.times do
+    unlucky_rand = rand(1...100)
+
+    @unlucky.push(unlucky_rand)
+  end 
+
+  render({ :template => "lottery_stuff/ohno.html.erb"})
+end
+
 end
